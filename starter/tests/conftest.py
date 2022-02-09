97da396f3b02f9155f89a8f6b5e7d43bb9c68d67
Raja Judeh <judeh.raja@outlook.com>
2022-02-09 19:05:43 +0200

    implement unit tests for model.py

---
+++ starter/tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+
+np.random.seed(42)
+
+
+@pytest.fixture(scope='session')
+def root_path():
+    root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+
+    return root_path
+
+
+@pytest.fixture(scope='session')
+def data(root_path):
+    df = pd.DataFrame(np.random.randint(0, 100, size=(100, 4), ), columns=list('ABCD'))
+    df['target'] = np.random.randint(0, 2, size=(100,))
+
+    y = df['target']
+    X = df.drop(['target'], axis=1)
+
+    return X, y
+
+
+@pytest.fixture(scope='session')
+def model(root_path, data):
+    X, y = data
+
+    model = LogisticRegression(random_state=42)
+    model.fit(X, y)
+
+    return model
---
+++ starter/tests/test_model.py
@@ -0,0 +1,30 @@
+from sklearn.linear_model import LogisticRegression
+from starter.ml.model import train_model, compute_model_metrics, inference
+
+
+def test_train_model(data):
+    X, y = data
+    model = train_model(X, y)
+
+    assert isinstance(model, LogisticRegression)
+    assert model.max_iter == 300
+    assert model.n_features_in_ == 4
+
+
+def test_inference(model, data):
+    X, y = data
+    y_pred = inference(model, X)
+
+    assert len(y_pred) == len(y)
+    assert y_pred.any() == 1
+
+
+def test_compute_model_metrics(model, data):
+    X, y = data
+    y_pred = inference(model, X)
+
+    precision, recall, fbeta = compute_model_metrics(y, y_pred)
+
+    assert precision > 0.5
+    assert recall > 0.5
+    assert fbeta > 0.5
