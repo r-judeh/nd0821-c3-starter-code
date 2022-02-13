10f1cb3ef5110d526f38e2061b0212aac7584244
Raja Judeh <judeh.raja@outlook.com>
2022-02-14 00:25:44 +0200

    add print the status code

--- request_live_app.py
+++ request_live_app.py
@@ -16,4 +16,5 @@ test_data.drop('salary')
 response = requests.post('https://udacity-census-inference-app.herokuapp.com/model_inference',
                          data=json.dumps(test_data.to_dict()))
 
+print(f"Status Code: {response.status_code}")
 print(f"Ground Truth: {label}, Response: {response.json()}")
